5433e7f9c621cd1d08542f37c08973e4c25aceb2
ynotopec <49277296+ynotopec@users.noreply.github.com>
2023-11-20 22:02:23 +0100

    Update install_k8s_gpu_ml_ai.sh
    example gpu config

--- install_k8s_gpu_ml_ai.sh
+++ install_k8s_gpu_ml_ai.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+# Variables
+adminMail=admin@example.com
+
 # Automated Installation Script for K8S GPU ML AI using MicroK8s
 
 # Function to enable MicroK8s addons
 enable_addons() {
-  microk8s enable helm3 dns community hostpath-storage ingress rbac cert-manager
+  microk8s enable helm3 dns community hostpath-storage ingress rbac
   
   # Check if a DHCP IP range is provided as the first argument ($1)
   if [ -n "$1" ]; then
@@ -14,6 +17,70 @@ enable_addons() {
 
   echo "Enabling GPU support..."
   microk8s enable gpu
+
+  echo "H100 GPU example"
+  cat <<EOT >~/time-slicing-config-fine.yaml
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: time-slicing-config-fine
+data:
+  h100-80gb: |-
+    version: v1
+    flags:
+      migStrategy: mixed
+    sharing:
+      timeSlicing:
+        resources:
+        - name: nvidia.com/gpu
+          replicas: 8
+#        - name: nvidia.com/mig-1g.10gb
+#          replicas: 4
+#        - name: nvidia.com/mig-1g.10gb+me
+#          replicas: 4
+#        - name: nvidia.com/mig-1g.20gb
+#          replicas: 4
+#        - name: nvidia.com/mig-3g.40gb
+#          replicas: 6
+#        - name: nvidia.com/mig-4g.40gb
+#          replicas: 8
+#        - name: nvidia.com/mig-7g.80gb
+#          replicas: 14
+EOT
+
+  kubectl create -n gpu-operator-resources -f ~/time-slicing-config-fine.yaml
+  kubectl patch clusterpolicy/cluster-policy \
+    -n gpu-operator-resources --type merge \
+    -p '{"spec": {"devicePlugin": {"config": {"name": "time-slicing-config-fine"}}}}'
+
+  #kubectl get events -n gpu-operator-resources --sort-by='.lastTimestamp'
+
+  kubectl label node \
+    --selector=nvidia.com/gpu.product=NVIDIA-H100-PCIe \
+    nvidia.com/device-plugin.config=h100-80gb
+
+  echo "Configure TLS Issuer"
+  microk8s enable cert-manager
+  mailExpire=${adminMail}
+  cat <<EOF |kubectl apply -f -
+apiVersion: cert-manager.io/v1
+kind: ClusterIssuer
+metadata:
+  name: letsencrypt-prod
+spec:
+  acme:
+    # L'URL de serveur ACME de Let's Encrypt pour la production
+    server: https://acme-v02.api.letsencrypt.org/directory
+    email: ${mailExpire}
+    privateKeySecretRef:
+      name: letsencrypt-prod
+    solvers:
+    - http01:
+        ingress:
+          class: public
+EOF
+
+
 }
 
 # Function to configure kubectl and set namespace
